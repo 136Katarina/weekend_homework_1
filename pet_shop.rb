5b341c98b95c9d33113d9128f95d4b6e7850f5ab
136Katarina <k.zemplenyiova@gmail.com>
2017-11-05 22:40:16 +0000

    tests running

--- pet_shop.rb
+++ pet_shop.rb
@@ -68,4 +68,29 @@ def find_pet_by_name(shop, pet_name)
       return pet
     end
   end
+  return nil
+end
+
+def remove_pet_by_name(shop, pet_name)
+    array_of_pets = shop[:pets]
+      pet_to_delete = find_pet_by_name(shop, pet_name)
+      array_of_pets.delete(pet_to_delete)
+end
+
+
+def add_pet_to_stock(shop, new_pet)
+    shop[:pets].push(new_pet)
+end
+
+def customer_pet_count(customer)
+    return customer[:pets].length
+end
+
+def add_pet_to_customer(customer, new_pet)
+    customer[:pets].push(new_pet)
+end
+
+
+def customer_can_afford_pet(hash_customer, hash_pet)
+  return hash_customer[:cash] > hash_pet[:price]
 end
